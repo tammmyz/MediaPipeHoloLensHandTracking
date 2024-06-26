9a982d6ac4dd2beae0c531407503fb5749a78bbe
Tammy Zeng <tammy.zeng908@gmail.com>
2024-06-26 15:13:27 -0400

    Set up webCamTexture in 3D space

--- Assets/Scripts/HandTracking.cs
+++ Assets/Scripts/HandTracking.cs
@@ -1,5 +1,5 @@
-using OpenCVForUnity.DnnModule;
-using OpenCVForUnity.UnityUtils;
+//using OpenCVForUnity.DnnModule;
+//using OpenCVForUnity.UnityUtils;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -12,8 +12,8 @@ public class HandTracking : MonoBehaviour
     private string PALM_DETECTION_MODEL_FILENAME = "OpenCVForUnity/dnn/palm_detection_mediapipe_2023feb.onnx";
     private string HANDPOSE_ESTIMATION_MODEL_FILENAME = "OpenCVForUnity/dnn/handpose_estimation_mediapipe_2023feb.onnx";
 
-    Net palmDetector;
-    Net handPoseEstimator;
+    //Net palmDetector;
+    //Net handPoseEstimator;
 
     [SerializeField]
     private Vector2Int requestedCameraSize = new(896, 504);
@@ -33,18 +33,20 @@ public class HandTracking : MonoBehaviour
 
     void Start()
     {
-        string palmDetectorFilepath = Utils.getFilePath(PALM_DETECTION_MODEL_FILENAME);
-        palmDetector = Dnn.readNet(palmDetectorFilepath);
-        palmDetector.setPreferableBackend(Dnn.DNN_BACKEND_OPENCV);
-        palmDetector.setPreferableTarget(Dnn.DNN_TARGET_CPU);
+        //string palmDetectorFilepath = Utils.getFilePath(PALM_DETECTION_MODEL_FILENAME);
+        //palmDetector = Dnn.readNet(palmDetectorFilepath);
+        //palmDetector.setPreferableBackend(Dnn.DNN_BACKEND_OPENCV);
+        //palmDetector.setPreferableTarget(Dnn.DNN_TARGET_CPU);
 
-        string handPoseEstimatorFilepath = Utils.getFilePath(HANDPOSE_ESTIMATION_MODEL_FILENAME);
-        handPoseEstimator = Dnn.readNet(handPoseEstimatorFilepath);
-        handPoseEstimator.setPreferableBackend(Dnn.DNN_BACKEND_OPENCV);
-        handPoseEstimator.setPreferableTarget(Dnn.DNN_TARGET_CPU);
+        //string handPoseEstimatorFilepath = Utils.getFilePath(HANDPOSE_ESTIMATION_MODEL_FILENAME);
+        //handPoseEstimator = Dnn.readNet(handPoseEstimatorFilepath);
+        //handPoseEstimator.setPreferableBackend(Dnn.DNN_BACKEND_OPENCV);
+        //handPoseEstimator.setPreferableTarget(Dnn.DNN_TARGET_CPU);
 
         webCamTexture = new WebCamTexture(requestedCameraSize.x, requestedCameraSize.y, cameraFPS);
         webCamTexture.Play();
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webCamTexture;
         StartTrackingHand();
     }
 
@@ -52,26 +54,26 @@ public class HandTracking : MonoBehaviour
     {
         await Task.Delay(1000);
 
-        actualCameraSize = new Vector2Int(webCamTexture.width, webCamTexture.height);
-        var renderTexture = new RenderTexture(handImageSize.x, handImageSize.y, 24);
-        if (debugRenderer != null && debugRenderer.gameObject.activeInHierarchy)
-        {
-            debugRenderer.material.mainTexture = renderTexture;
-        }
-        while (true)
-        {
-            var cameraTransform = CopyCameraTransForm(Camera.main);
-            Graphics.Blit(webCamTexture, renderTexture);
-            await Task.Delay(32);
+        //actualCameraSize = new Vector2Int(webCamTexture.width, webCamTexture.height);
+        //var renderTexture = new RenderTexture(handImageSize.x, handImageSize.y, 24);
+        //if (debugRenderer != null && debugRenderer.gameObject.activeInHierarchy)
+        //{
+        //    debugRenderer.material.mainTexture = renderTexture;
+        //}
+        //while (true)
+        //{
+        //    var cameraTransform = CopyCameraTransForm(Camera.main);
+        //    Graphics.Blit(webCamTexture, renderTexture);
+        //    await Task.Delay(32);
 
-            var texture = ToTexture2D(renderTexture);
-            await Task.Delay(32);
+        //    var texture = ToTexture2D(renderTexture);
+        //    await Task.Delay(32);
             
-            // add task here for object recognition
+        //    // add task here for object recognition
 
-            Destroy(texture);
-            Destroy(cameraTransform.gameObject);
-        }
+        //    Destroy(texture);
+        //    Destroy(cameraTransform.gameObject);
+        //}
     }
 
     private Transform CopyCameraTransForm(Camera camera)
